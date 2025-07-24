713134a873a8bdf4f02c872acf11b98a7e028171
Shigeo Tokua <gazikit@gmail.com>
2025-07-24 17:14:13 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 // Đổi repo gốc ở đây 1 lần
-const BASE_PATH = '/lib/'; // ví dụ '/libv2/' nếu đổi tên
+const BASE_PATH = ''; // ví dụ '/lib/' nếu đổi tên
 
 // Thêm thẻ <base> động vào <head>
 const base = document.createElement('base');
