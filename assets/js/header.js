bc03aa7707f1fefe639498322d38949b1915ac93
Shigeo Tokua <gazikit@gmail.com>
2025-07-30 10:05:42 +0700

    update

---
+++ assets/js/header.js
@@ -0,0 +1,22 @@
+const toggle = document.querySelector('.menu-toggle');
+const navList = document.querySelector('.nav ul');
+const backdrop = document.querySelector('.menu-backdrop');
+
+if (toggle && navList && backdrop) {
+  toggle.addEventListener('click', () => {
+    navList.classList.toggle('show');
+    backdrop.classList.toggle('show');
+  });
+
+  backdrop.addEventListener('click', () => {
+    navList.classList.remove('show');
+    backdrop.classList.remove('show');
+  });
+
+  navList.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', () => {
+      navList.classList.remove('show');
+      backdrop.classList.remove('show');
+    });
+  });
+}
