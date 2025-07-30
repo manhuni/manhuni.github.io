478263cfa6c9d2432e737e8ce1a9e59cfa2c7733
Shigeo Tokua <gazikit@gmail.com>
2025-07-30 14:45:49 +0700

    update

--- main/projects/cocos/recreate-uuid/assets/js/recreate-uuid-slideshow.js
+++ main/projects/cocos/recreate-uuid/assets/js/recreate-uuid-slideshow.js
@@ -1,19 +1,13 @@
 const slideshow1_images = [
   "/main/projects/cocos/recreate-uuid/assets/images/demo/structure-folder.jpg",
   "/main/projects/cocos/recreate-uuid/assets/images/demo/panel-location.jpg",
-  "/main/projects/cocos/recreate-uuid/assets/images/demo/panel-locations.jpg",
-];
-const slideshow2_images = [
-  "/main/projects/cocos/recreate-uuid/assets/images/demo/structure-folder.jpg",
-];
+  "/main/projects/cocos/recreate-uuid/assets/images/demo/editor.jpg",
+]; 
 
 window.addEventListener("DOMContentLoaded", () => {
   initPhotoViewer();
   createSlideshow("#slideshow1", slideshow1_images, {
     onImageClick: (src, index) => openPhotoViewerByIndex(index, slideshow1_images)
-  });
-  createSlideshow("#slideshow2", slideshow2_images, {
-    onImageClick: (src, index) => openPhotoViewerByIndex(index, slideshow2_images)
-  });
+  }); 
 
 });
